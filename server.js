03ae5e628daef4c0b969812904495be717b07bbc
Criste Phillips <criste.phillips@yahoo.com>
2022-02-02 21:06:13 -0500

    validation

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 // require connection to express.js
 const express = require('express');
 
@@ -10,7 +13,11 @@ const PORT = process.env.PORT || 3001;
 // instantiate the server (represent the abstraction of the server); express() assigned so we can later chain methods to Express.js server
 const app = express();
 
-
+// tell Express.js app to intercept POST request before it gets to callback fxn
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 // add before the .get() callback, create a filter functionality
 function filterByQuery(query, animalsArray) {
@@ -53,6 +60,39 @@ function findById(id, animalsArray) {
     return result;
 }
 
+// POSTing a new animal causes it to be be imported to animals array from animals.json file
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    // return finished code to post route for response
+    return animal;
+}
+
+// validation functionality
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+
+    if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+        return false;
+    }
+
+    return true;
+}
+
 // add the .get() callback route for app.listen()
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -66,13 +106,30 @@ app.get('/api/animals', (req, res) => {
 
 
 // add a 2nd .get() to filter more closely
-app.get('/api/animals/:id', (req, res) => {
+app.get('/api/animals/:id', (req, res) => {1
     const result = findById(req.params.id, animals);
     if (result) {
         res.json(result);
     } else {
         res.sendStatus(404);
     }
+});
+
+// set up a rount on server that accepts data to be used or stored server-side; .post means that we defined the route to listen for POST requests (not GET requests)
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be; set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is in incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        
+    // add animal to json file and animals array in this fxn
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(req.body);
+  }
 });
 
 // chain listen() method onto our server to make the server listen; 3001 used as it is commonly used & does not require special permissions to use
