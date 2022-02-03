0cab8e00accf420d30ee44fefdfae6351022efdd
Criste Phillips <criste.phillips@yahoo.com>
2022-02-03 15:40:15 -0500

    additional routing

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express');
 // require the data that is requested by the front-end code
 const { animals } = require('./data/animals');
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 // tell heroku to use port process environment
 const PORT = process.env.PORT || 3001;
 
@@ -20,24 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+// have app use router set up in apiRoutes or HTML routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// route to index.html; ('/') connects to root route of server
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-// wildcard ('*') always goes last in line of app.get functionality, otherwise it will take precedence over all the others
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
 // chain listen() method onto our server to make the server listen; 3001 used as it is commonly used & does not require special permissions to use; this should ALWAYS BE LAST
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
